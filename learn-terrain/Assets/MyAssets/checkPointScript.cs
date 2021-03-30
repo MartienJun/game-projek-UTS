7dd2990b0935f9263b8ce0e9afa3d2cff276a320
MartienJun <martienjunaedi123@gmail.com>
2021-03-30 20:00:40 +0700

    Adding respawn system

---
+++ learn-terrain/Assets/MyAssets/checkPointScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class checkPointScript : MonoBehaviour
+{
+    playerDataScript player;
+    
+    void OnTriggerEnter(Collider other)
+    {
+        player = GameObject.FindObjectOfType<playerDataScript>();
+        player.checkPoint = gameObject.GetComponent<checkPointScript>();
+    }
+
+    public void spawn()
+    {
+        player.transform.position = transform.position;
+    }
+}   
---
+++ learn-terrain/Assets/MyAssets/playerDataScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerDataScript : MonoBehaviour
+{
+    public checkPointScript checkPoint;
+
+    void Update()
+    {
+        if(transform.position.y < -2)
+        {
+            checkPoint.spawn();
+        }
+    }
+}
