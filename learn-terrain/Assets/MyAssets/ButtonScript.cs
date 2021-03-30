e326a77a4e31c3a7c6d13240464913229fd08409
MartienJun <martienjunaedi123@gmail.com>
2021-03-30 20:27:24 +0700

    Add button script

---
+++ learn-terrain/Assets/MyAssets/ButtonScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonScript : MonoBehaviour
+{
+    public GameObject ExitMenu;
+
+    public void ExitApp() 
+    { 
+        Application.Quit(); 
+    } 
+    
+    public void ChangeScreen(string scene_name) 
+    { 
+        SceneManager.LoadScene(scene_name); 
+    } 
+}
